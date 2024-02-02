e48829822eb8442dac0c56c6310904fd84c03bf9
tiagoJC1 <tiagocuello679@gmail.com>
2024-02-02 15:33:28 -0300

    subi el footer

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const port = 3030;
 
 app.use(express.static('public'));
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './views/home.html'))
+// })
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))
+    res.sendFile(path.join(__dirname, './views/footer.html'))
 })
 
 
@@ -16,4 +20,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(port, () => console.log(`http://localhost${port}`));
+app.listen(port, () => console.log(`http://localhost:${port}`));
+
+
