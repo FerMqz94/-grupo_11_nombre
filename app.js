d1d570927c2d9cd6a6631ba7f3dfa519188c1948
FerMqz94 <fernando.mqz94@email.com>
2024-02-02 20:43:25 -0300

    Modifique el archvo general.css y cree las variables

--- app.js
+++ app.js
@@ -6,19 +6,9 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './views/home.html'))
-// })
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/footer.html'))
+     res.sendFile(path.join(__dirname, './views/index.html'))
 })
-
-
-
-
-
-
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
 
