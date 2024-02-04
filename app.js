9af5218b9ce700266b847574f4118933516d75d6
tiagoJC1 <tiagocuello679@gmail.com>
2024-02-04 20:09:43 -0300

    nada y algo

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.use(express.static('public'));
     // res.sendFile(path.join(__dirname, './views/home.html'))
 // })
 
-app.get('/carrito', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/carrito.html'))})
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/header.html'))})
+// app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, './views/header.html'))}) 
 
 
 
