a0cd68b0c317b847dafdca0574557e7f36243a04
FerMqz94 <fernando.mqz94@email.com>
2024-02-04 19:59:08 -0300

    Cambie las imagenes de las capsulas por unas con mejor resolucion

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, './views/index.html'))
+     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
