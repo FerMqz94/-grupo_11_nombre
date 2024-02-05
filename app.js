f6756d39a213da7d07e0986831e24db9aea26b40
tiagoJC1 <tiagocuello679@gmail.com>
2024-02-05 16:35:57 -0300

    solucionando errores

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))
+    res.sendFile(path.join(__dirname,'./views/home.html'))
 });
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
+    res.sendFile(path.join(__dirname,'./views/carrito.html'))
 });
-app.get('/producto-detalle', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+app.get('/  ', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/productDetail.html'))
 });
 
 
