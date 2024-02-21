aaf9f1dc8137c3d30ec37501236c9b070943fc37
tiagoJC1 <tiagocuello679@gmail.com>
2024-02-21 18:18:06 -0300

    para revisar mis error

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 const port = 3030;
 
+// CONFIGURACIONES
+
+app.set("view engine" , "ejs")
+app.set("views" ,path.join(__dirname,"./views"))
+
+// MEDIA
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/home.html'))
-});
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/carrito.html'))
-});
-app.get('/producto-detalle', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/productDetail.html'))
-});
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'))
-});
-app.get('/ingreso', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'))
-});
+// RUTAS 
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'./views/home.html'))
+// });
+// app.get('/carrito', (req, res) => {
+//     res.sendFile(path.join(__dirname,'./views/carrito.html'))
+// });
+// app.get('/producto-detalle', (req, res) => {
+//     res.sendFile(path.join(__dirname,'./views/productDetail.html'))
+// });
+// app.get('/registro', (req, res) => {
+//     res.sendFile(path.join(__dirname, './views/register.html'))
+// });
+// app.get('/ingreso', (req, res) => {
+//     res.sendFile(path.join(__dirname, './views/login.html'))
+// });
 
+// ROTERS
+const otherRoutes = require("./routes/other.routes")
+
+// ROUTES 
+app.use("/", otherRoutes)
+
+//  SERVER
 app.listen(port, () => console.log(`http://localhost:${port}`));
 
 
---
+++ controller/other/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    home: require("./home.controller"),
+    login: require("./login.controller"),
+    carrito: require("./carrito.controller"),
+    register: require("./register.controller"),
+    productDetail: require("./productDetail.controller"),
+  }
