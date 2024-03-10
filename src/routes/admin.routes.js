8b8437e851b9dfd7bad8d8f32ca179ebb7874bc0
FerMqz94 <fernando.mqz94@email.com>
2024-03-10 19:29:40 -0300

    Se agregan archivos para eliminar productos

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct, editProduct, listProducts} = require("../controllers/admin");
+const { createProduct, editProduct, listProducts, deleteProduct, removeProduct} = require("../controllers/admin");
 
 // "/admin"
 router.get("/productos", listProducts); 
 router.get('/crear-productos', createProduct);
 router.get('/editar-productos', editProduct);
+router.get('/eliminar-producto/:id', deleteProduct);
+router.delete('/eliminar-producto/:id', removeProduct);
+router.get('/eliminar-producto/:id', deleteProduct);
+router.delete('/eliminar-producto/:id', removeProduct)
 
 module.exports = router;
