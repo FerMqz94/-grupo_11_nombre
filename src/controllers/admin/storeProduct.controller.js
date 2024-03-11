3ae4359b57730c6ff52a6ffdf6a22951fc5d6d09
VERAPAO <pmnvera1984@gmail.com>
2024-03-11 18:27:34 -0300

    Agregué lógica al controller storeProduct

--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -2,6 +2,26 @@ const { loadData, saveData } = require('../../database');
 
 module.exports = (req, res) => {
     const products = loadData();
-    const { name, description, featuredDescription, category, colors, price, size } = req.body;
+    const { name, description, featuredDescription, category, colors, price, size, neworsale, available } = req.body;
+    const newID = products[products.length -1].id + 1;
+
+    const newProduct = {
+        id: newID,
+        name: name.trim(),
+        description: description.trim(),
+        featuredDescription: featuredDescription.trim(),
+        category:,
+        colors:,
+        price: +price,
+        size:,
+        new:
+        sale:,
+        available:,
+
+        
+    }
+
+
+
     res.redirect('/admin/productos')
 }
