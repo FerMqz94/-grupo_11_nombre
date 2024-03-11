eaba9bbd380eab91a986541881456091cd529e79
VERAPAO <pmnvera1984@gmail.com>
2024-03-11 14:01:55 -0300

    Cree la ruta y los controladores para guardar el producto

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct, editProduct, listProducts, deleteProduct, removeProduct} = require("../controllers/admin");
+const { createProduct, storeProduct, editProduct, listProducts, deleteProduct, removeProduct} = require("../controllers/admin");
 
 // "/admin"
 router.get("/productos", listProducts); 
 router.get('/crear-productos', createProduct);
+router.post('/crear-productos', storeProduct)
 router.get('/editar-productos', editProduct);
 router.get('/eliminar-producto/:id', deleteProduct);
 router.delete('/eliminar-producto/:id', removeProduct);
