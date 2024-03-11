8f21d4bc4476686d360382d5297fae26d5eb0df8
VERAPAO <pmnvera1984@gmail.com>
2024-03-11 18:02:14 -0300

    Agregué destructuración de propiedades en el controlador

--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -1,6 +1,7 @@
 const { loadData, saveData } = require('../../database');
 
 module.exports = (req, res) => {
-
+    const products = loadData();
+    const { name, description, featuredDescription, category, colors, price, size } = req.body;
     res.redirect('/admin/productos')
 }
