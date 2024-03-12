569fc2ee9814f361a0d92537d33591d5c4ba6007
VERAPAO <pmnvera1984@gmail.com>
2024-03-12 10:18:39 -0300

    Terminé la lógica que permite construir un array de talles y otros de colores en los respectivas propiedades

--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -1,29 +1,102 @@
 const { loadData, saveData } = require('../../database');
+const path = require('path');
+const fs = require('fs');
+const colorsFile = require('../../database');
+
+//TRAIGO Y LEO LA INFORMACIÓN DE COLORS.JSON
+const loadDataColors = (filename = "colors") => {
+const pathJSONColors = path.join(__dirname, `../../database/${filename}.json`);
+const dataJSONColors = fs.readFileSync(pathJSONColors, 'utf-8');
+const dataJSColors = JSON.parse(dataJSONColors); 
+return dataJSColors   
+}
 
 module.exports = (req, res) => {
-    const products = loadData();
-    const { name, description, featuredDescription, category, colors, price, size, neworsale, available } = req.body;
+
+    //VARIABLES Y CONSTANTES PARA OPERAR CON LOS DATOS
+    let products = loadData();
+    let colors = loadDataColors();
+    const { name, description, featuredDescription, category, price, talle1, talle2, talle3, talle4, talle5, black, beige, blue, white, red, green, purple, orange, lightblue, gray, lavender, pink, silver, bluishGreen, gold, neworsale, available } = req.body;
     const newID = products[products.length -1].id + 1;
+    let talleBooleano;
+    let tallesSend = [];
+    let colorsName;
+    let sendColor = []
+    let colorArrayBoolean = [];
+    let colorBoolean;
 
-    res.send(req.body)
+    //////FUNCIONES PARA COLORES//////
 
-   // const newProduct = {
-      //  id: newID,
-     //   name: name.trim(),
-    //    description: description.trim(),
-        featuredDescription: featuredDescription.trim(),
-      //  category:,
-        //colors:,
-        //price: +price,
-      //  size:,
-        //new: neworsale === "new",
-       // sale: neworsale === "sale"
-        //available:,
+    //GENERAR ARRAY DE BOOLEANOS CON LA iNFORMACIÓN QUE VIENE DESDE EL BODY SI ES "ON" VA A SER TRUE SI ES UNDEFINED FALSE
+    const booleanColors = (color, colorArrayBoolean) => {
+        colorBoolean = color === "on"? true : false;
+        return colorArrayBoolean.push(colorBoolean);
+    }
 
-        
-    //}
+    // EJECUCIÓN
+    booleanColors(black, colorArrayBoolean)
+    booleanColors(beige, colorArrayBoolean);
+    booleanColors(blue,  colorArrayBoolean);
+    booleanColors(white, colorArrayBoolean);
+    booleanColors(red, colorArrayBoolean);
+    booleanColors(green, colorArrayBoolean);
+    booleanColors(purple, colorArrayBoolean);
+    booleanColors(orange, colorArrayBoolean);
+    booleanColors(lightblue, colorArrayBoolean);
+    booleanColors(gray, colorArrayBoolean);
+    booleanColors(lavender, colorArrayBoolean);
+    booleanColors(pink, colorArrayBoolean);
+    booleanColors(silver, colorArrayBoolean);
+    booleanColors(bluishGreen, colorArrayBoolean),
+    booleanColors(gold, colorArrayBoolean)
+
+    // FUNCIÓN PARA GENERAR UN ARRAY CON LOS NOMBRES QUE VIENEN COMO TRUE, LOS DEMÁS SON DESCARTADOS - SE EJECUTA GRACIAS A LA INFORMACIÓN QUE SE OBTIENE DESDE EL COLORS.JSON 
+    const storeColors = (colorArrayBoolean, colors) => {
+
+       for( let i = 0; i < colors.length; i++){
+        if(colorArrayBoolean[i] === true){
+            colorsName = colors[i].name;
+            sendColor.push(colorsName);     
+        } 
+       }
+       return sendColor;
+    }
+
+    //EJECUCIÓN DE LA FUNCIÓN 
+    storeColors(colorArrayBoolean, colors);
+    
+    ///// FUNCIÓN PARA TALLES////
+
+    const storeTalles = (talle, tallesSend) => {
+        talleBooleano = talle === "on"? true : false;
+        return tallesSend.push(talleBooleano);
+    }
+
+    // EJECUCIÓN
+    storeTalles(talle1, tallesSend);
+    storeTalles(talle2, tallesSend)
+    storeTalles(talle3, tallesSend);
+    storeTalles(talle4, tallesSend);
+    storeTalles(talle5, tallesSend);
 
 
+    const newProduct = {
+       id: newID,
+       name: name.trim(),
+       description: description.trim(),
+       featuredDescription: featuredDescription.trim(),
+       category: category?.trim(),
+       talles: tallesSend,
+       colors: sendColor,
+       price: +price,
+       new: neworsale === "new",
+       sale: neworsale === "sale",
+       available: available === "on"? true: false
+ 
+    }
 
-    res.redirect('/admin/productos')
+    products = [...products, newProduct];
+    saveData(products);
+
+    res.redirect("/admin/productos");
 }
