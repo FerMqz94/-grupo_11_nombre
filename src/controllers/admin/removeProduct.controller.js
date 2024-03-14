07df2b678b3dc9cd2734e3ccb9dc5cf76bafffd0
VERAPAO <pmnvera1984@gmail.com>
2024-03-13 22:04:29 -0300

    Hice un upgrade del código de uno de los controllers

--- src/controllers/admin/removeProduct.controller.js
+++ src/controllers/admin/removeProduct.controller.js
@@ -1,8 +1,9 @@
-const { saveData } = require('../../database')
-const products = require('../../database/products.json')
+const { saveData, loadData } = require('../../database')
 
 module.exports = (req,res) => {
     const {id} = req.params
+
+    const products = loadData()
 
     const productLessOne = products.filter(p => p.id !== +id)
 
