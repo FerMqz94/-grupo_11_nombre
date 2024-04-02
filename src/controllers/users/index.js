2216c1efcb5b89461bdca7dbb944bb4c6642c596
VERAPAO <pmnvera1984@gmail.com>
2024-04-02 03:02:15 -0300

    Comencé de manera muy incipiente la lógica para el update de perfil

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     perfilUsuario: require("./perfilUsuario.controller"),
-  
+    updateUsers: require('./updateUsers.controller')
   }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { perfilUsuario } = require("../controllers/users");
-
+const { updateUsers } = require('../controllers/users/updateUsers.controller')
 
 
 
 //  /users/perfil-usuario
 router.get("/perfil-usuario/:id", perfilUsuario);
+/*router.put("/perfil-usuario/:id", updateUsers);*/
 
 
 module.exports = router;
