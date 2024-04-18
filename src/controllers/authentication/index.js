5ebf06167ccfc2c11adb2779fc9ab85d07512718
FerMqz94 <fernando.mqz94@email.com>
2024-04-18 20:16:59 -0300

    Agrega logout al index

--- src/controllers/authentication/index.js
+++ src/controllers/authentication/index.js
@@ -2,6 +2,6 @@ module.exports = {
     register: require("../authentication/register.controller"),
     login: require("../authentication/login.controller"),
     processLogin: require("./processLogin.controller"),
-    processRegister: require("./processRegister.controller")
-    
+    processRegister: require("./processRegister.controller"),
+    logout: require("./logout.controller")   
 }
