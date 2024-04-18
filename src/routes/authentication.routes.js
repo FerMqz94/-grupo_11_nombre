86b112c42dd81e7b325fccb8c20c80dd63c366ff
FerMqz94 <fernando.mqz94@email.com>
2024-04-18 20:12:26 -0300

    Intentando crear la logia de cerrar sesion

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { login, register, processRegister, processLogin } = require("../controllers/authentication");
+const { login, register, processRegister, processLogin, logout } = require("../controllers/authentication");
 const {registerValidation} = require("../middlewares/validations/register.validation");
 const { loginValidation } = require("../middlewares/validations/login.validation");
 
@@ -12,6 +12,8 @@ router.post("/iniciar", loginValidation, processLogin);
 //  /autenticacion/registrar
 router.get("/registrar", register);
 router.post("/registrar", registerValidation, processRegister);
+
+router.get("/cerrar-sesion", logout)
 
 
 
