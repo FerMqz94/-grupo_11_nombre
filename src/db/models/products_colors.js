23e6c25abf69540be3c93d2a904a5c090684f42b
FerMqz94 <fernando.mqz94@email.com>
2024-04-27 16:32:16 -0300

    Se crean 4 tablas y sus respectivas migraciones

---
+++ src/db/models/products_colors.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products_Colors extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Products_Colors.init({
+    id_product: DataTypes.INTEGER,
+    id_color: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Products_Colors',
+  });
+  return Products_Colors;
+};
