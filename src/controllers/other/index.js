2de4b65689a96b9949763c77c66eb5717a394d5c
Alexander036 <alexanderbejarano036@gmail.com>
2024-04-29 20:53:52 -0300

    Probando la card para lista de productos del cliente

--- src/controllers/other/index.js
+++ src/controllers/other/index.js
@@ -3,8 +3,6 @@ const productListController = require("../products/List.controller");
 module.exports = {
     home: require("./home.controller"),
     search: require("./search.controller"),
-<<<<<<< HEAD
+
     aboutUs: require("./aboutUs.controller")
-=======
->>>>>>> 3d276f3e6ab069ff723ff4adec402278911584ed
   }
--- src/routes/other.routes.js
+++ src/routes/other.routes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const { home, search, aboutUs } = require("../controllers/other");
-=======
-const { home, search, List } = require("../controllers/other");
->>>>>>> 3d276f3e6ab069ff723ff4adec402278911584ed
 
 router.get("/", home);
 // router.get("/home", (req, res) => res.redirect("/"));
 router.get("/resultados", search);
-<<<<<<< HEAD
 
 router.get("/sobre-nosotros", aboutUs);
 
-=======
->>>>>>> 3d276f3e6ab069ff723ff4adec402278911584ed
 module.exports = router;
