1519149d08e150fabd0136f37eb21c14c236a9ff
FerMqz94 <fernando.mqz94@email.com>
2024-05-04 18:49:22 -0300

    Se mejoro la logica de la tabla pivot products_sizes

--- src/db/seeders/20240430182146-products_sizes.js
+++ src/db/seeders/20240430182146-products_sizes.js
@@ -1,30 +1,27 @@
 'use strict';
 
-const sizesJSON = require("../../database/sizes.json")
-const productsJSON = require("../../database/products.json")
+const productsJSON = require("../../database/products.json");
 
 
-const sizesDBMapped = productsJSON.map((p) => {
-const sizes = p.sizes.map(siz => {
+const existingProductIds = new Set([1,2,3,4,5]);
+
+const sizesDBMapped = productsJSON.flatMap((product) => {
+  
+  if (!existingProductIds.has(product.id)) return [];
+  return product.sizes.map((sizeId) => {
     return {
-      
-    }
-  })   
-  return sizes;  
-}).flat(1)
-
-
+      id_product: product.id,
+      id_size: sizeId,
+    };
+  });
+});
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
-   await queryInterface.bulkInsert('Products_Sizes', sizesDBMapped, {});
-  
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('products_sizes', sizesDBMapped, {});
   },
-  async down (queryInterface, Sequelize) {    
-
-    await queryInterface.bulkDelete('Products_Sizes', null, {});
-    
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('products_sizes', null, {});
   }
 };
