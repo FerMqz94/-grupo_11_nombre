1c448251df2b43e1c42187e2b814f29e2752151c
gaston <gastoncastillo1920@gmail.com>
2024-05-04 16:16:14 -0300

    probando factorizar las vista y login

--- src/controllers/authentication/processLogin.controller.js
+++ src/controllers/authentication/processLogin.controller.js
@@ -33,3 +33,47 @@ module.exports = (req, res) => {
 
   }
 };
+
+// const bcrypt = require("bcryptjs");
+// const { validationResult } = require("express-validator");
+// const db = require("../../db/models");
+
+// module.exports = (req, res) => {
+//   const { email, password, remember } = req.body;
+//   // const users = loadData("users");
+
+//   const userFind = users.find((u) => u.email === email);
+//   const errors = validationResult(req);
+//   db.User.findOne({
+//     where: {
+//       email,
+//     },
+//     include:["role"]
+//   }).then((user) => {
+  
+//     if (!user) res.send("El usuario no existe");
+
+//     const isPasswordValid = bcrypt.compareSync(password, user?.password);
+
+//     if (!isPasswordValid) res.send("El password es incorrecto");
+
+//     req.session.userLogin = {
+//       id: user.id,
+//       name: user.name,
+//       surname: user.surname,
+//       avatar: user.avatar,
+//       role: user.role.name,
+//     };
+
+//     if (remember)
+//       res.cookie("userLogin", req.session.userLogin, { maxAge: 6000 * 30 });
+
+//     // res.redirect("/");
+//     //    } else {
+
+//    res.render("./authentication/login", {
+//      old: req.body,
+//     errors: errors.mapped(),
+//   });
+//   });
+// };
--- src/controllers/products/productDetail.controller.js
+++ src/controllers/products/productDetail.controller.js
@@ -14,3 +14,27 @@ module.exports = (req, res) => {
         res.redirect("products/vista-no-encontrada")
     }
 }
+
+// const db = require("../../db/models");
+
+// module.exports = (req, res)=>{
+//   const { id } = req.params;
+//   db.product.findByPk(id, {
+//     include: [images],
+//   }).then((product)=>{
+//   const productsFind = products.find(p => p.id === +id) 
+//   if (productsFind) {
+    
+//   res.render("products/productDetail", {product: productsFind, toThousand, products  } )
+//   } else {
+//     const image = req.file
+//       res.redirect("products/vista-no-encontrada")
+//   }
+// })
+
+
+//     res.render("/id",{
+//       product
+//     });
+//   });
+// };
