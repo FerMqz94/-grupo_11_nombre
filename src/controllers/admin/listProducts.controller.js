7578163ad83550dd748a7f7889bad64f73dd699a
Alexander036 <alexanderbejarano036@gmail.com>
2024-05-06 18:54:20 -0300

    Corrigiendo detalles del updateProduct y de editProduct.ejs

--- src/controllers/admin/listProducts.controller.js
+++ src/controllers/admin/listProducts.controller.js
@@ -1,51 +1,35 @@
-// const { loadData } = require('../../database')
-
-// module.exports = (req, res) => {
-//   const products = loadData();
-//   res.render("admin/listProducts", { 
-//     products
-//    }, (err, content) => {
-
-//     err && res.send(err.message)
-//     res.render("partials/dashboard", {
-//       contentView: content
-//      })
-// })};
-
-
 const db = require("../../db/models");
 
 module.exports = (req, res) => {
-
-  db.products.findAll({
-    include:[
-    {
-        association:"images",
-        attributes:['file'],
-        },
-    {
-        association:"colors" ,
-        attributes:['name']
-    },
-    {
-        association:"sizes",
-        attributes:['sizes']
-    }
-    ],
-})
-.then((products)=>{
-        res.render("admin/listProducts", { 
-            products
-           }, 
-           (err, content) => {
-    
-                err && res.send(err.message)
-                res.render("partials/dashboard", {
-                  contentView: content
-                 })
-})
-.catch((err) => {
-    res.send(err.message)
-})
-    
-    })};
+    const productsPromise = db.Product.findAll({
+        include: [
+            { 
+                model: db.Colors,
+                as: 'products'
+            },
+            { 
+                model: db.Sizes,
+                as: 'products'
+            }
+        ]
+    });
+    const categoriesPromise = db.Categories.findAll();
+    Promise.all([categoriesPromise, productsPromise])
+        .then(([categories, products]) => {
+            res.render("admin/listProducts", {
+                categories,
+                products
+            }, (err, content) => {
+                if (err) {
+                    res.send(err.message);
+                } else {
+                    res.render("partials/dashboard", {
+                        contentView: content
+                    });
+                }
+            });
+        })
+        .catch((err) => {
+            res.send(err.message);
+        });
+};
