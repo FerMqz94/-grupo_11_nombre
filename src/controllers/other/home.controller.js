69b080ea0d2c6a218ba9941e5b9fb0001c144cce
tiagoJC1 <tiagocuello679@gmail.com>
2024-05-12 18:15:23 -0300

    solucione error del home haciendo que traiga los productos

--- src/controllers/other/home.controller.js
+++ src/controllers/other/home.controller.js
@@ -1,12 +1,26 @@
 // const { loadData } = require("../../database");
 const db = require("../../db/models");
 module.exports = (req,res) => {
- db.Product.findAll()
+    db.Product.findAll({
+        // attributes: 
+        // {
+        //     exclude: [
+        //       "createdAt",
+        //       "updatedAt",
+        //     ],
+        //   },
+          include: [
+            {
+              association: "images",
+            //   attributes: ["file"],
+            },
+          ],
+    })
  .then((products)=>{
 //  return res.send(products)
 
-  
+  res.render("./other/home", {products})
 })
- res.render("./other/home",)
+ 
   }
 
