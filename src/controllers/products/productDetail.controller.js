be86140e5d423620319967a82b5966bd61a20498
Alexander036 <alexanderbejarano036@gmail.com>
2024-05-13 10:42:53 -0300

    Modificando productDetail.ejs y productDetail.controller.js

--- src/controllers/products/productDetail.controller.js
+++ src/controllers/products/productDetail.controller.js
@@ -15,31 +15,57 @@
 //     }
 // }
 
-const db = require("../../db/models");
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+// const db = require("../../db/models");
+// const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-module.exports = (req, res)=>{
-  const { id } = req.params;
+// module.exports = (req, res)=>{
+//   const { id } = req.params;
   
-  db.Product.findByPk(id, {
-    include: [images],
+//   db.Product.findByPk(id, {
+//     include: ["images", "category", "color", "sizes"]
   
-  }).then((products)=>{
+//   }).then((products)=>{
   
-  const productsFind = products.find(p => p.id === +id) 
-  if (productsFind) {
+//   const productsFind = products.find(p => p.id === +id) 
+//   if (productsFind) {
     
-  res.render("products/productDetail", {product: productsFind, toThousand, products  } )
-  } else {
+//   res.render("products/productDetail", {product: productsFind, toThousand, products  } )
+//   } else {
 
-    console.log(product);
-      res.redirect("products/vista-no-encontrada")
-  }
-})
+//     console.log(product);
+//       res.redirect("products/vista-no-encontrada")
+//   }
+// })
 
 
-    res.render("/id",{
-      product
-    });
-  }
-  
+//     res.render("/id",{
+//       product
+//     });
+//   }
+
+module.exports = (req, res) => {
+  const { id } = req.params;
+
+  db.Product.findByPk(id, {
+      include: ["images", "category", "color", "sizes"]
+  })
+  .then(product => {
+      if (product) {
+          db.Product.findAll({
+              include: ["images"]
+          })
+          .then(products => {
+              res.render("products/productDetail", { product, products });
+          })
+          .catch(error => {
+              res.redirect("/products/vista-no-encontrada");
+          });
+      } else {
+          res.redirect("/products/vista-no-encontrada");
+      }
+  })
+  .catch(error => {
+      console.error("Error al buscar el producto:", error);
+      res.redirect("/products/vista-no-encontrada");
+  });
+};
