affdc5e2ff6123b66a2164b2a0e79c3ad04859f2
FerMqz94 <fernando.mqz94@email.com>
2024-06-03 20:14:58 -0300

    hace la ruta de contactos

--- src/controllers/other/index.js
+++ src/controllers/other/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     home: require("./home.controller"),
     search: require("./search.controller"),
-    aboutUs: require("./aboutUs.controller")
+    aboutUs: require("./aboutUs.controller"),
+    contactUs: require("./contactUs.controller")
   }
--- src/routes/other.routes.js
+++ src/routes/other.routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { home, search, aboutUs } = require("../controllers/other");
+const { home, search, aboutUs, contactUs } = require("../controllers/other");
 
 router.get("/", home);
+
 // router.get("/home", (req, res) => res.redirect("/"));
+
 router.get("/resultados", search);
 
 router.get("/sobre-nosotros", aboutUs);
+
+router.get("/contacto", contactUs);
 
 module.exports = router;
