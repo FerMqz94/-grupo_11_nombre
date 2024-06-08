bd331de000195b7e990d4fe143d8565665d568f3
gaston <gastoncastillo1920@gmail.com>
2024-06-07 21:39:58 -0300

    modificando list controller

--- src/controllers/authentication/processLogin.controller.js
+++ src/controllers/authentication/processLogin.controller.js
@@ -1,39 +1,3 @@
-// const { loadData } = require("../../database");
-// const bcrypt = require("bcryptjs");
-// const { validationResult } = require("express-validator");
-
-// module.exports = (req, res) => {
-//   const { email, password, recordame} = req.body;
-//   const users = loadData("users");
-//   const errors = validationResult(req);
-
-//   const userFind = users.find((u) => u.email === email);
-
-//   if(errors.isEmpty()){
-
-//       req.session.userLogin = {
-//       id: userFind.id, 
-//       name: userFind.name,
-//       username: userFind.username,
-//       rol: userFind.rol,    
-//       avatar: userFind.avatar
-  
-//       };
-
-//        if(recordame ) res.cookie("userLogin", req.session.userLogin, {maxAge: 6000 * 30 * 100})
-  
-//        res.redirect("/")
-
-//   } else {
-
-//     res.render("./authentication/login", {
-//       old: req.body,
-//       errors: errors.mapped(),
-//     });
-
-//   }
-// };
-
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 const db = require("../../db/models");
