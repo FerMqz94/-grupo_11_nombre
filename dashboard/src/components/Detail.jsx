807c033291a2f8b08c50a32a104fe09f7aaaeb9b
gaston <gastoncastillo1920@gmail.com>
2024-06-08 23:09:02 -0300

    intalando react y insertando dshboard

---
+++ dashboard/src/components/Detail.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+
+function Detail  ()  {
+  return (
+    <div>
+        {/* <Link to="Detail"></Link>
+           <Route path="/Detail" component = {Detail }/> */}
+      <a
+                  className="btn btn-danger"
+                  target="_blank"
+                  rel="nofollow"
+                  href="/"
+                >
+                  View movie detail
+                </a>
+    </div>
+  )
+}
+
+export default Detail
