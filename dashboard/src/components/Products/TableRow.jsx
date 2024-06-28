b8a1c0f88e1e4cc54e575ea4f93121f61b75fd0d
gaston <gastoncastillo1920@gmail.com>
2024-06-28 01:11:24 -0300

    provando rutas

--- dashboard/src/components/Products/TableRow.jsx
+++ dashboard/src/components/Products/TableRow.jsx
@@ -6,6 +6,7 @@ export const TableRow = ({ id,title, duration, rating, genre, awards }) => {
     <tr>
       <td><Link to={`/productos/${id}`}>{id}</Link></td>
       <td><Link to={`/usuarios/${id}`}>{id}</Link></td>
+      <td><Link to={`/graficos/${id}`}>{id}</Link></td>
       <td>Opcion</td>
       <td><h3>Opcion 1</h3></td>
       <td><h3>Opcion 2</h3></td>
--- dashboard/src/routes/RoutesProvider.jsx
+++ dashboard/src/routes/RoutesProvider.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       },
       ...routesProducts,
       ... routesUsers,
+      ... routesGraficos,
     ],
   },
 ]);
